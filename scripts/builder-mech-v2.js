c54c0dafa45b6ebd7a934d8b93658d432473b9d3
Fresh791 <77986574+Fresh791@users.noreply.github.com>
2021-02-11 17:02:06 +0100

    Update builder-mech-v2.js

--- scripts/builder-mech-v2.js
+++ scripts/builder-mech-v2.js
@@ -1,5 +1,4 @@
-const ais = this.global.mcu.ai;
-
 const bmv2 = extendContent(UnitType, "builder-mech-v2,{});
 bmv2.constructor = () => extend(UnitAirMove, {});
 bmv2.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 6, 60 * 9, 64));
+bmv2.ammoType = AmmoTypes.power;
