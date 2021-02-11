753816c9df666c581c0a0416695a75c8d6cb0157
Fresh791 <77986574+Fresh791@users.noreply.github.com>
2021-02-11 18:28:25 +0100

    Update carrier-flagship.js

--- scripts/carrier-flagship.js
+++ scripts/carrier-flagship.js
@@ -1,6 +1,6 @@
 const CfS1 = extendContent(UnitType, "carrier-flagship", {});
 CfS1.constructor = () => extend(MechUnit, {});
-CfS1.abilities.add(new ForceFieldAbility(80, 5, 2000, 60 * 20));
+CfS1.abilities.add(new ForceFieldAbility(20, 5, 2000, 60 * 20));
 CfS1.abilities.add(new UnitSpawnAbility(UnitTypes.mono, 5 * 60, 58 / 4, 54 / 4));
 CfS1.abilities.add(new UnitSpawnAbility(UnitTypes.flare, 5 * 60, 58 / 4, 54 / 7));
 CfS1.abilities.add(new UnitSpawnAbility(UnitTypes.horizon, 5 * 60, 58 / 4, 54 / 4));
