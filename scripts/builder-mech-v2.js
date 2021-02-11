7d86ac31aad10e65d4872bdc307a1c98a4d3a80f
Fresh791 <77986574+Fresh791@users.noreply.github.com>
2021-02-11 16:59:23 +0100

    Add files via upload

---
+++ scripts/builder-mech-v2.js
@@ -0,0 +1,5 @@
+const ais = this.global.mcu.ai;
+
+const bmv2 = extendContent(UnitType, "builder-mech-v2,{});
+bmv2.constructor = () => extend(UnitAirMove, {});
+bmv2.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 6, 60 * 9, 64));
