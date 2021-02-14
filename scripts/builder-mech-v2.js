e721b2966c26733879c3355e9924c5883f166272
Fresh791 <77986574+Fresh791@users.noreply.github.com>
2021-02-14 09:21:24 +0100

    Delete builder-mech-v2.js

--- scripts/builder-mech-v2.js
+++
@@ -1,5 +0,0 @@
-const BmV2 = extendContent(UnitType, "builder-mech-v2", {});
-BmV2.constructor = () => extend(MechUnit, {});
-BmV2.abilities.add(new ForceFieldAbility(12, 0.13, 260, 80 * 5));
-BmV2.abilities.add(new ShieldRegenFieldAbility(8, 120, 80, 10));
-BmV2.ammoType = AmmoTypes.power;
