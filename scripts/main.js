996a31c700a49f33dd45c9eadedb5adef6c86cfe
Fresh791 <77986574+Fresh791@users.noreply.github.com>
2021-03-21 16:34:02 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 require("carrier-flagship");
 require("weathers");
+require("blockhealthreader");
 //require("planets");
 Events.on(ContentInitEvent, () => {
 	print("[Mindustry Extended] loading finished!")
