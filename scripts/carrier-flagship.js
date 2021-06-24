d0585460142276b3c34a607a2eb8d969b4b322fc
Fresh791 <77986574+Fresh791@users.noreply.github.com>
2021-06-24 13:20:18 +0200

    Delete carrier-flagship.js

--- scripts/carrier-flagship.js
+++
@@ -1,7 +0,0 @@
-const CfS1 = extendContent(UnitType, "carrier-flagship", {});
-CfS1.constructor = () => extend(MechUnit, {});
-CfS1.abilities.add(new ForceFieldAbility(20, 5, 2000, 60 * 20));
-CfS1.abilities.add(new UnitSpawnAbility(UnitTypes.mono, 5 * 60, 0 / 4, -54 / 4));
-CfS1.abilities.add(new UnitSpawnAbility(UnitTypes.flare, 5 * 60, 0 / 4, -54 / 7));
-CfS1.abilities.add(new UnitSpawnAbility(UnitTypes.horizon, 5 * 62, 0 / 4, -54 / 4));
-CfS1.ammoType = AmmoTypes.powerHigh;
