8e0cae9ad15d6575458117925772a98876d4e3c5
Fresh791 <77986574+Fresh791@users.noreply.github.com>
2021-07-28 18:54:14 +0200

    Update gold-exchanger.js

--- scripts/gold-exchanger.js
+++ scripts/gold-exchanger.js
@@ -1,21 +1,32 @@
 const goldItem = extend(Block, "gold-exchanger", {
   init(){
     this.super$init();
-    outputs = {};
-    outputs[Items.surgeAlloy] = 5
-    outputs[Items.copper] = 10
+
+    this.outputs = {};
+    this.outputs[Items.surgeAlloy] = 5;
+    this.outputs[Items.copper] = 10;
+    this.itemAccepts = [Items.copper, Items.surgeAlloy];
+    this.craftTimer = this.timers++;
   }
 });
 
 goldItem.buildType = extend(Building, goldItem, {
   selection: Items.copper,
+
   buildConfiguration(table){
-    ItemSelection.buildTable(table, Vars.content.items(), () -> this.selection, e -> this.configure);
+    ItemSelection.buildTable(table, this.block.itemAccepts, () -> this.selection, e -> this.configure);
   },
+
+  acceptItem(source, item){
+    return item in this.block.outputs;
+  },
+
   updateTile(){
     this.super$updateTile();
+
     var gold = Vars.content.get(ContentType.item, "gold");
-    if(this.selection != null && this.items.has(gold) && this.items.get(gold) >= 1){
+
+    if(this.timer(this.block.craftTimer, 60) && this.selection != null && this.items.has(gold) && this.items.get(gold) >= 1){
       this.items.remove(gold, 1);
       this.items.add(this.selection, this.block.outputs[selection]);
     }
