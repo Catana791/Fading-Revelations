076b0a1c0bedb808e384fcb607a6de2abb426a74
Fresh791 <77986574+Fresh791@users.noreply.github.com>
2021-08-10 23:17:04 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
 require("outpost");
 require("gold-exchanger");
 require("music");
+
+Events.on(ClientLoadEvent, e => {
+  Time.run(8, () -> {
+    a = Core.scene.find("menu container");
+    cont = a.children.get(0);
+    cont.row();
+    cont.button("discord", () => {
+      Core.app.openURI("https://discord.gg/YB3ky9tJR4");
+    });
+  });
+}); 
