4c9116babcf1a1991e6f80b7a372f3e834c833d4
Fresh791 <77986574+Fresh791@users.noreply.github.com>
2021-08-16 12:41:32 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,13 @@ require("outpost");
 //require("gold-exchanger");
 require("music");
 
-let cont;
-Events.on(ClientLoadEvent, e => {
-  Time.run(10, () => {
-    cont = Vars.ui.discord.cont.children.get(2);
-    cont.row();
-    cont.button("Mindustry Extended Discord", () => Core.app.openURI("https://discord.gg/YB3ky9tJR4"));
-  }
-} 
+//let cont;
+//Events.on(ClientLoadEvent, e => {
+//  Time.run(10, () => {
+    //cont = Vars.ui.discord.cont.children.get(2);
+    //cont.row();
+   // cont.button("Mindustry Extended Discord", () => Core.app.openURI("https://discord.gg/YB3ky9tJR4"));
+//  }
+//} 
 
  
