235434221c6e24dc0732858039989e3f45afb1ea
Fresh791 <77986574+Fresh791@users.noreply.github.com>
2022-07-29 20:41:16 +0200

    Create serpulo.js

---
+++ scripts/serpulo.js
@@ -0,0 +1,7 @@
+const valleyOfDespair = new SectorPreset("valleyOfDespair", Planets.serpulo, 7);
+valleyOfDespair.captureWave = 100;
+valleyOfDespair.difficulty = 10;
+
+module.exports = {
+  valleyOfDespair: valleyOfDespair,
+};
