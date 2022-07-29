4cbf5bf83f626d0c06f37054db2d0a37dedef011
Fresh791 <77986574+Fresh791@users.noreply.github.com>
2022-07-29 21:10:20 +0200

    Update serpulo.js

--- scripts/campaign/serpulo.js
+++ scripts/campaign/serpulo.js
@@ -1,6 +1,7 @@
 const valleyOfDespair = new SectorPreset("valleyOfDespair", Planets.serpulo, 7);
 valleyOfDespair.captureWave = 100;
 valleyOfDespair.difficulty = 10;
+valleyOfDespair.localizedName = "Valley of Despair";
 
 module.exports = {
   valleyOfDespair: valleyOfDespair
