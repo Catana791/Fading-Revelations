27a8ce0455dfeb0a37f30d26256b953d4291387e
Fresh791 <77986574+Fresh791@users.noreply.github.com>
2022-08-02 20:16:56 +0200

    Fixing console errors

--- scripts/campaign/serpulo.js
+++ scripts/campaign/serpulo.js
@@ -1,8 +1,8 @@
-const valleyOfDespair = new SectorPreset("valleyOfDespair", Planets.serpulo, 7);
-valleyOfDespair.captureWave = 100;
-valleyOfDespair.difficulty = 10;
-valleyOfDespair.localizedName = "Valley of Despair";
+//const valleyOfDespair = new SectorPreset("valleyOfDespair", Planets.serpulo, 7);
+//valleyOfDespair.captureWave = 100;
+//valleyOfDespair.difficulty = 10;
+//valleyOfDespair.localizedName = "Valley of Despair";
 
-module.exports = {
-  valleyOfDespair: valleyOfDespair
-};
+//module.exports = {
+  //valleyOfDespair: valleyOfDespair
+//};
