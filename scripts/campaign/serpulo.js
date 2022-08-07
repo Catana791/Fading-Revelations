be6099c361712a27115a5b2d9a10408c82e87689
Fresh791 <77986574+Fresh791@users.noreply.github.com>
2022-08-07 14:30:15 +0200

    Update serpulo.js

--- scripts/campaign/serpulo.js
+++ scripts/campaign/serpulo.js
@@ -1,4 +1,4 @@
-const sector = extend(SectorPreset, "valleyOfDespair", Planets.serpulo, 7) {
+const valleyOfDespair = extend(SectorPreset, "valleyOfDespair", Planets.serpulo, 7) {
 localizedName: "Valley of Depair",
 difficulty: 10,
 captureWave: 100
