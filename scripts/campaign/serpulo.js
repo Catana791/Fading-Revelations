acd70366822902c47cb781b534549c0009dc53c9
Fresh791 <77986574+Fresh791@users.noreply.github.com>
2022-08-07 15:32:16 +0200

    Update serpulo.js

--- scripts/campaign/serpulo.js
+++ scripts/campaign/serpulo.js
@@ -1,10 +1,10 @@
-const valleyOfDespair = new SectorPreset("valleyOfDespair", Planets.serpulo, 7);
-valleyOfDespair.captureWave = 100;
-valleyOfDespair.difficulty = 10;
-valleyOfDespair.localizedName = "Valley of Despair";
-valleyOfDespair.alwaysUnlocked = true;
-valleyOfDespair.preset = "valleyOfDespair";
+//const valleyOfDespair = new SectorPreset("valleyOfDespair", Planets.serpulo, 7);
+//valleyOfDespair.captureWave = 100;
+//valleyOfDespair.difficulty = 10;
+//valleyOfDespair.localizedName = "Valley of Despair";
+//valleyOfDespair.alwaysUnlocked = true;
+//valleyOfDespair.preset = "valleyOfDespair";
 
-module.exports = {
-  valleyOfDespair: valleyOfDespair
-};
+//module.exports = {
+  //valleyOfDespair: valleyOfDespair
+//};
